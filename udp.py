edf7c7f5923d10710732c50a19bb1e478a340f38
gokutheengineer <engineerzubizaretta@gmail.com>
2023-10-30 13:59:22 +0300

    working prototype including chat

--- udp.py
+++ udp.py
@@ -5,7 +5,6 @@ MAX_SIZE_BYTES_UDP = 65535
 def server(port):
     # Create a socket object for UDP connection with IPv4 addressing
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #port = 3000
     hostname = '127.0.0.1'
     s.bind((hostname, port))
     print('Server is listening on port {}'.format(s.getsockname()))
@@ -18,18 +17,57 @@ def server(port):
         # Send data to client
         s.sendto(data, clientAddress)
         
-def client(port):
+def server_chat(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    message = input('input your message in lowercase: ')
-    data = message.encode('ascii')
-    s.sendto(data, ('127.0.0.1', port))
-    print('The OS assigned the address {} to me'.format(s.getsockname())) 
-    data, address = s.recvfrom(MAX_SIZE_BYTES_UDP)
-    reply = data.decode('ascii')
-    print('The server {} replied with {!r}'.format(address, reply))
+    hostname = '127.0.0.1'
+    s.bind((hostname, port))
+    print('Server is listening on port {}'.format(s.getsockname()))
+    while True:
+        # Receive data from client
+        data, clientAddress = s.recvfrom(MAX_SIZE_BYTES_UDP)
+        message = data.decode('ascii')
+        print('The client at {} says {!r}'.format(clientAddress, message))
+        msg_to_client = input('input your message in lowercase to client: ')
+        data = msg_to_client.encode('ascii')
+        s.sendto(data, clientAddress)
+        
+def client_multi(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    hosts = []
+    while True:
+        host = input('input host address: ')
+        if host == 'q':
+            break
+        hosts.append((host, port))
+        message = input('input your message in lowercase: ')
+        data = message.encode('ascii')
+        s.sendto(data, (host, port))
+        print('The OS assigned the address {} to me'.format(s.getsockname())) 
+        data, address = s.recvfrom(MAX_SIZE_BYTES_UDP)
+        reply = data.decode('ascii')
+        if address in hosts:
+                print('The server {} replied with {!r}'.format(address, reply))
+                hosts.remove(address)
+        else:
+            print('Message {!r} from unexpected host {!r}'.format(reply, address))
+ 
+ 
+def client_chat(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    host = '127.0.0.1'
+    while True:
+        s.connect((host, port))
+        print('The OS assigned the address {} to me'.format(s.getsockname())) 
+        message = input('input your message in lowercase: ')
+        data = message.encode('ascii')
+        s.send(data)
+        data = s.recv(MAX_SIZE_BYTES_UDP)
+        reply = data.decode('ascii')
+        print('The server replied with {!r}'.format(reply))
+    
     
 if __name__ == '__main__':
-    funcs = {'client': client, 'server': server}
+    funcs = {'client': client_chat, 'server': server_chat}
     parser = argparse.ArgumentParser(description='UDP client and server')
     parser.add_argument('functions', choices=funcs, help='client or server')
     parser.add_argument('-p', metavar='PORT', type=int, default=3000, help='UDP port (default 3000)')
